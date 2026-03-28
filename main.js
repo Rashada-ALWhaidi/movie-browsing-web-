9fa4347e18f3b888f4c0ffa93ca22a8d145ea111
Rashada AL Whaidi <rashadaalwhaidi@gmail.com>
2026-03-28 20:54:28 +0300

    feat: setup basic server

---
+++ main.js
@@ -0,0 +1,13 @@
+import http from 'http';
+
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'HELLO' }));
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port :${PORT}`);
+});
